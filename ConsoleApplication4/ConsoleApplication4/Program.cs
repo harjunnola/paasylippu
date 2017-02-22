e328796ad5c34b33ad1a9c3beb1e91d87d2ad801
harjunnola <harri@harrijunnola.com>
2017-02-22 10:28:50 +0200

    kaksi lukua

---
+++ ConsoleApplication4/ConsoleApplication4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int Number;
+            Console.WriteLine("anna luku:");
+            Number = int.Parse(Console.ReadLine());
+
+            int Number2;
+            Console.WriteLine("anna luku2:");
+            Number2 = int.Parse(Console.ReadLine());
+
+            float kerroin = 2;
+
+
+            if (Number < 0)
+            {
+                Console.WriteLine("väärä numeroarvo");
+            }
+                        
+            {
+                Console.WriteLine("Lukusi ovat {0}", Number * kerroin);
+            }
+                Console.WriteLine("Lukusi ovat {0},{0}", Number2 * kerroin);
+
+            Console.ReadLine();
+        }
+    }
+}
